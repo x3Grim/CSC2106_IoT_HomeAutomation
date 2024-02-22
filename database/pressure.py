c77812e001ed68b0e25a3b77a77c27ecd81f4f78
x3Grim <petergohcodes@gmail.com>
2024-02-22 16:12:19 +0800

    Pressure DB completed

--- database/pressure.py
+++ database/pressure.py
@@ -1,5 +1,6 @@
 import pymongo
-import datetime
+import pytz
+from datetime import datetime
 import calendar
 
 # Setup pressure collection
@@ -7,48 +8,74 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
 mycol = mydb["pressure_readings"]
 
-def add_many():
-    mylist = [
-        { "pressure": 80, "datetime": datetime_to_epoch(2024, 2, 22, 21, 00, 0) },
-        { "pressure": 60, "datetime": datetime_to_epoch(2024, 2, 23, 00, 00, 0) },
-        { "pressure": 50, "datetime": datetime_to_epoch(2024, 2, 23, 3, 00, 0) },
-        { "pressure": 80, "datetime": datetime_to_epoch(2024, 2, 23, 6, 00, 0) },
-    ]
+def add_one(new_document):
+    mycol.insert_one(new_document)
+    cursor = mycol.find({})
+    for document in cursor:
+        print(document)
+# usage: add_one({ "pressure": 80, "datetime": datetime_to_epoch() })
 
-    mycol.insert_many(mylist)
+
+def add_many(new_list):
+    mycol.insert_many(new_list)
 
     cursor = mycol.find({})
     for document in cursor:
         print(document)
+# usage: add_many([
+    #     { "pressure": 0, "datetime": datetime_to_epoch() },
+    #     { "pressure": 50, "datetime": datetime_to_epoch() },
+    #     { "pressure": 75, "datetime": datetime_to_epoch() },
+    #     { "pressure": 100, "datetime": datetime_to_epoch() },
+    # ])
 
 def del_all():
     x = mycol.delete_many({})
-    print(x.deleted_count, " documents deleted.") 
+    print(x.deleted_count, " documents deleted.")
+# deletes all documents
+
+def del_one(del_document):
+    mycol.delete_one(del_document)
+
 
 def retrieve_all():
     cursor = mycol.find({})
     return cursor
+# returns list of documents, still need to loop
+
+def retrieve_latest():
+    latest_document = mycol.find_one(sort=[('_id', pymongo.DESCENDING)])
+    print(latest_document)
 
 def update_doc(old_values, new_values):
     mycol.update_one(old_values, { "$set": new_values })
     cursor = mycol.find({})
     for document in cursor:
         print(document)
+# usage: update_doc({ "pressure": 100, "datetime": datetime_to_epoch() }, { "pressure": 0, "datetime": datetime_to_epoch() })
 
 def process_pressure():
-    pressureList = retrieve_all()
-    for pr in pressureList:
-        print("Pressure: " + str(pr['pressure']))
-        print("Date Time: " + str(datetime.datetime.fromtimestamp(pr['datetime'])))
-        if pr['pressure'] < 0:
-            print("Invalid reading")
-        elif pr['pressure'] > 100:
-            print("User is asleep")
-        elif pr['rate'] == 0:
-            print("User is dead")
+    pressure_list = retrieve_all()
+    for pressure in pressure_list:
+        print(pressure['_id'])
+        print("Pressure: " + str(pressure['pressure']))
+        print("Date Time: " + str(datetime.fromtimestamp(pressure['datetime']))) # convert epoch to datetime utc
+        if pressure['pressure'] < 0:
+            print("Invalid pressure value")
+        elif pressure['pressure'] > 0:
+            print("User is on the bed")
+        elif pressure['pressure'] > 250:
+            print("User has fallen asleep")
+# for processing retrieved data
 
-def datetime_to_epoch(year, month, date, hour, minute, second):
-    epoch_datetime = datetime.datetime(year, month, date, hour, minute, second)
-    return calendar.timegm(epoch_datetime.timetuple())
+def datetime_to_epoch():
+    utc_now = datetime.utcnow()
+    utc_timezone = pytz.timezone('UTC')
+    utc_now = utc_timezone.localize(utc_now)
+    sg_timezone = pytz.timezone('Asia/Singapore')
+    sg_now = utc_now.astimezone(sg_timezone)
+    epoch_time = calendar.timegm(sg_now.utctimetuple())
+    return epoch_time
+# convert current time to epoch
 
-process_pressure()
+retrieve_latest()
