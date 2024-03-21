b18991c2e00bc3e77f4657fa9d12c8b167f7dea2
ACED07 <awchengen007@gmail.com>
2024-03-21 16:24:53 +0800

    heartrate

--- heartbeat/heartrate/heartrate.ino
+++ heartbeat/heartrate/heartrate.ino
@@ -19,60 +19,10 @@ void loop()
   radar.Breath_Heart(); // Breath and heartbeat information output
 
   if (radar.sensor_report != 0x00) {
-    switch (radar.sensor_report) {
-      case HEARTRATEVAL:
-        Serial.print("Sensor monitored the current heart rate value is: ");
-        Serial.println(radar.heart_rate, DEC);
-        Serial.println("----------------------------");
-        break;
-      case HEARTRATEWAVE: // Valid only when real-time data transfer mode is on
-        Serial.print("The heart rate waveform(Sine wave) -- point 1: ");
-        Serial.print(radar.heart_point_1);
-        Serial.print(", point 2 : ");
-        Serial.print(radar.heart_point_2);
-        Serial.print(", point 3 : ");
-        Serial.print(radar.heart_point_3);
-        Serial.print(", point 4 : ");
-        Serial.print(radar.heart_point_4);
-        Serial.print(", point 5 : ");
-        Serial.println(radar.heart_point_5);
-        Serial.println("----------------------------");
-        break;
-      case BREATHNOR:
-        Serial.println("Sensor detects current breath rate is normal.");
-        Serial.println("----------------------------");
-        break;
-      case BREATHRAPID:
-        Serial.println("Sensor detects current breath rate is too fast.");
-        Serial.println("----------------------------");
-        break;
-      case BREATHSLOW:
-        Serial.println("Sensor detects current breath rate is too slow.");
-        Serial.println("----------------------------");
-        break;
-      case BREATHNONE:
-        Serial.println("There is no breathing information yet, please wait...");
-        Serial.println("----------------------------");
-        break;
-      case BREATHVAL:
-        Serial.print("Sensor monitored the current breath rate value is: ");
-        Serial.println(radar.breath_rate, DEC);
-        Serial.println("----------------------------");
-        break;
-      case BREATHWAVE: // Valid only when real-time data transfer mode is on
-        Serial.print("The breath rate waveform(Sine wave) -- point 1: ");
-        Serial.print(radar.breath_point_1);
-        Serial.print(", point 2 : ");
-        Serial.print(radar.breath_point_2);
-        Serial.print(", point 3 : ");
-        Serial.print(radar.breath_point_3);
-        Serial.print(", point 4 : ");
-        Serial.print(radar.breath_point_4);
-        Serial.print(", point 5 : ");
-        Serial.println(radar.breath_point_5);
-        Serial.println("----------------------------");
-        break;
-    }
+      Serial.print("Sensor monitored the current heart rate value is: ");
+      Serial.println(radar.heart_rate, DEC);
+      Serial.println("----------------------------");
   }
-  delay(200); // Add time delay to avoid program jam
+  delay(500); // Add time delay to avoid program jam
 }
+
