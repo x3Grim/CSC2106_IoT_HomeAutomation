81b427acdd6e2ef6aa5da16ceeb5ea799ebef4e5
ACED07 <awchengen007@gmail.com>
2024-03-25 15:46:43 +0800

    motion pullup

--- motion/motion.ino
+++ motion/motion.ino
@@ -1,42 +1,33 @@
-// Include required libraries
 #include "Arduino.h"
 
-// Define pin numbers for serial communication
-#define LD2420_RX_PIN 20
-#define LD2420_TX_PIN 21
-#define LD2420_PRESENCE_PIN 5
+// Define pin numbers for LD2420 sensor
+#define LD2420_PRESENCE_PIN 10
 
-// Create a HardwareSerial object to communicate with the LD2420 sensor
-HardwareSerial ld2420Serial(1); // Use Serial1 for ESP32C3 Super Mini
+// Other LD2420 configuration variables
+const unsigned long presenceTimeout = 120000; // 120s in milliseconds
+const int minGateDistance = 1;
+const int maxGateDistance = 12;
+const float gateStillSensitivity = 0.5;
+const float gateMoveSensitivity = 0.5;
 
 void setup() {
   // Initialize serial communication
   Serial.begin(115200);
-  ld2420Serial.begin(115200, SERIAL_8N1, LD2420_RX_PIN, LD2420_TX_PIN);
-
+  
   // Set the Presence Signal Output pin
-  pinMode(LD2420_PRESENCE_PIN, INPUT);
-
-  // Initialize LD2420 sensor
-  initializeLD2420();
+  pinMode(LD2420_PRESENCE_PIN, INPUT_PULLUP);
 }
 
 void loop() {
   // Check for motion detection
   if (motionDetected()) {
     Serial.println("Motion detected!");
+  } else {
+    Serial.println("No motion detected!");
   }
-
+  
   // Delay before next iteration
   delay(1000);
-}
-
-// Function to initialize LD2420 sensor
-void initializeLD2420() {
-  // Send any initialization commands if needed
-  // For example, setting operating mode
-  // You can send commands via UART to configure the sensor
-  Serial.println("Initialize!");
 }
 
 // Function to check for motion detection
